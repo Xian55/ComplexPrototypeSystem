445b5ad27d9cdc8f1c0774f4c4cf0f9c6f2324ac
Xian55 <367101+Xian55@users.noreply.github.com>
2022-10-17 20:02:09 +0200

    Service: Added Config to store Sensor related setting such as GUID, Interval. Added ConfigDAO to manage life time and accessing Config data.

---
+++ Service/DAO/ConfigDAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.Json;
+
+using System.IO;
+
+using ComplexPrototypeSystem.Service.Data;
+
+using Microsoft.Extensions.Logging;
+
+namespace ComplexPrototypeSystem.Service.DAO
+{
+    public class ConfigDAO
+    {
+        private readonly ILogger<ConfigDAO> logger;
+
+        public Config Config { get; }
+
+        public ConfigDAO(ILogger<ConfigDAO> logger)
+        {
+            this.logger = logger;
+
+            string fileName = Path.Join(AppDomain.CurrentDomain.BaseDirectory, ConfigMeta.FileName);
+
+            if (!File.Exists(fileName))
+            {
+                Config = new Config();
+
+                Save();
+
+                logger.LogInformation($"Config created {fileName}");
+            }
+            else
+            {
+                string json = File.ReadAllText(fileName);
+                Config = JsonSerializer.Deserialize<Config>(json);
+            }
+
+            logger.LogInformation($"Config loaded id: {Config.Id} | interval: {Config.Interval}ms");
+        }
+
+        public void Save()
+        {
+            string fileName = Path.Join(AppDomain.CurrentDomain.BaseDirectory, ConfigMeta.FileName);
+            string json = JsonSerializer.Serialize(Config);
+            File.WriteAllText(fileName, json);
+            logger.LogInformation("Config saved");
+        }
+    }
+}
---
+++ Service/Data/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ComplexPrototypeSystem.Service.Data
+{
+    public static class ConfigMeta
+    {
+        public const string FileName = "config.json";
+    }
+
+    public class Config
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public int Interval { get; set; } = 5000;
+    }
+}
